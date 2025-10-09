35a467d31228c9f79f3757d2947f82dc1a49f7f7
Ioann Yaromenka <ioann.yaromenka@innowise.com>
2025-10-09 23:27:27 +0300

    Added the optional prototype scope beans

--- src/main/java/com/intern/javacore/Main.java
+++ src/main/java/com/intern/javacore/Main.java
@@ -1,5 +1,6 @@
 package com.intern.javacore;
 
+import com.intern.javacore.minispring.components.DummyPrototypeClass;
 import com.intern.javacore.minispring.framework.MiniApplicationContext;
 import com.intern.javacore.minispring.components.DummyClass;
 
@@ -10,6 +11,14 @@ public class Main {
                 new MiniApplicationContext("com.intern.javacore.minispring.components");
         DummyClass myClass = ctx.getBean(DummyClass.class);
 
-        System.out.println(myClass.getDumb("Dependency Injection"));;
+        // afterPropertiesSet() is called for each
+        DummyPrototypeClass prototype1 = ctx.getBean(DummyPrototypeClass.class);
+        DummyPrototypeClass prototype2 = ctx.getBean(DummyPrototypeClass.class);
+
+        System.out.println(prototype1.hashCode() == prototype2.hashCode()); // false
+        System.out.println(prototype1.equals(prototype2)); // false
+
+
+        System.out.println(myClass.getDumb("Dependency Injection"));
     }
 }
--- src/main/java/com/intern/javacore/minispring/framework/MiniApplicationContext.java
+++ src/main/java/com/intern/javacore/minispring/framework/MiniApplicationContext.java
@@ -10,19 +10,41 @@ import java.util.stream.Collectors;
 public class MiniApplicationContext {
 
     private final Map<Class<?>, Object> beans = new HashMap<>();
+    private final Set<Class<?>> prototypeBeans = new HashSet<>();
 
     public MiniApplicationContext(String basePackage) {
         try {
             Set<Class<?>> componentClasses = scanPackageForComponents(basePackage);
-            instantiateComponents(componentClasses);
-            injectFields();
+            extractPrototypeBeans(componentClasses);
+            instantiateSingletons(componentClasses);
+            injectFieldsForSingletons();
             invokeInitializingBeans();
         } catch (Exception e) {
             throw new RuntimeException("Failed to initialize context for package: " + basePackage, e);
         }
     }
 
+    private void extractPrototypeBeans(Set<Class<?>> componentClasses) {
+        for (Class<?> clazz : componentClasses) {
+            if (clazz.isAnnotationPresent(Scope.class)
+                    && "prototype".equals(clazz.getAnnotation(Scope.class).value())) {
+                prototypeBeans.add(clazz);
+            }
+        }
+        componentClasses.removeAll(prototypeBeans);
+    }
+
     public <T> T getBean(Class<T> type) {
+
+        if (prototypeBeans.contains(type)) {
+            try {
+                return type.cast(getPrototypeBean(type));
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+                throw new RuntimeException("Failed to instantiate the prototype bean: " + type);
+            }
+        }
+
         // direct match
         Object direct = beans.get(type);
         if (direct != null) {
@@ -42,36 +64,53 @@ public class MiniApplicationContext {
         }
     }
 
-    private void instantiateComponents(Set<Class<?>> componentClasses) throws Exception {
+    private Object getPrototypeBean(Class<?> clazz) throws Exception {
+        Object instance = clazz.getDeclaredConstructor().newInstance();
+        injectFields(clazz, instance);
+        if (instance instanceof InitializingBean) {
+            try {
+                ((InitializingBean) instance).afterPropertiesSet();
+            } catch (Exception e) {
+                throw new RuntimeException("Failed during afterPropertiesSet for bean: " + instance.getClass(), e);
+            }
+        }
+
+        return instance;
+    }
+
+    private void instantiateSingletons(Set<Class<?>> componentClasses) throws Exception {
         for (Class<?> clz : componentClasses) {
             Object instance = clz.getDeclaredConstructor().newInstance();
             beans.put(clz, instance);
         }
     }
 
-    private void injectFields() throws IllegalAccessException {
+    private void injectFieldsForSingletons() throws IllegalAccessException {
         for (Map.Entry<Class<?>, Object> entry : Collections.unmodifiableSet(beans.entrySet())) {
             Class<?> clz = entry.getKey();
             Object instance = entry.getValue();
+            injectFields(clz, instance);
+        }
+    }
 
-            for (Field field : clz.getDeclaredFields()) {
-                if (field.isAnnotationPresent(Autowired.class)) {
-                    Class<?> fieldType = field.getType();
-                    Object dependency = resolveDependency(fieldType);
-                    if (dependency == null) {
-                        Autowired aut = field.getAnnotation(Autowired.class);
-                        if (aut.required()) {
-                            throw new IllegalStateException("Required dependency not found for field: "
-                                    + field + " in " + clz);
-                        } else {
-                            continue;
-                        }
+    private void injectFields(Class<?> clz, Object instance) throws IllegalAccessException {
+        for (Field field : clz.getDeclaredFields()) {
+            if (field.isAnnotationPresent(Autowired.class)) {
+                Class<?> fieldType = field.getType();
+                Object dependency = resolveDependency(fieldType);
+                if (dependency == null) {
+                    Autowired aut = field.getAnnotation(Autowired.class);
+                    if (aut.required()) {
+                        throw new IllegalStateException("Required dependency not found for field: "
+                                + field + " in " + clz);
+                    } else {
+                        continue;
                     }
-                    boolean accessible = field.canAccess(instance);
-                    field.setAccessible(true);
-                    field.set(instance, dependency);
-                    field.setAccessible(accessible);
                 }
+                boolean accessible = field.canAccess(instance);
+                field.setAccessible(true);
+                field.set(instance, dependency);
+                field.setAccessible(accessible);
             }
         }
     }
